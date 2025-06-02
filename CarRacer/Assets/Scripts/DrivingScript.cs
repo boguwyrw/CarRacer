98befd05b3bc2a191df77be97c1b9e8437a7ac4e
Bogumil Wyrwal <b.wyrwal@wp.pl>
2025-06-02 16:31:29 +0200

    Preparing the car model

--- CarRacer/Assets/Scripts/DrivingScript.cs
+++ CarRacer/Assets/Scripts/DrivingScript.cs
@@ -12,11 +12,25 @@ public class DrivingScript : MonoBehaviour
     public Rigidbody rb;
     public float currentSpeed;
 
+    [SerializeField] private GameObject backLightL;
+    [SerializeField] private GameObject backLightR;
+
     public void Drive(float acceleration, float brake, float steer)
     {
         acceleration = Mathf.Clamp(acceleration, -1f, 1f);
         steer = Mathf.Clamp(steer, -1f, 1f) * maxSteerAngle; // pomno¿yæ przez wartoœci
         brake = Mathf.Clamp01(brake) * maxBrakeTorque; // pomno¿yæ przez wartoœci
+
+        if (brake != 0)
+        {
+            backLightL.SetActive(true);
+            backLightR.SetActive(true);
+        }
+        else
+        {
+            backLightL.SetActive(false);
+            backLightR.SetActive(false);
+        }
 
         float thrustTorque = 0f;
 
